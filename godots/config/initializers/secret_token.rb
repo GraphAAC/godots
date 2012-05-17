f5214251d053fea11e864eb9adc5b7ca4ce066de
Graph <rg321@yandex.ru>
2012-05-17 04:41:18 -0400

    Initial commit

---
+++ godots/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Godots::Application.config.secret_token = '0ee8b9a5e84aad3e99c6a1a1c2bf1cba7bf6bd2f4fddc75ee33d3acebb40d11c522c79149ded12df305f5ca4460b00c5a4dc11998e98f63ac8c1b8cca457ac6d'
